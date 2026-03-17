4a4e84fd53080d1f12392e2558b6f1ecce3ac0dd
Wayne Bishop <waynewbishop@users.noreply.github.com>
2026-03-16 17:25:31 -0700

    Initial Commit

---
+++ quiver-charts-validation-app/quiver_charts_validation_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  quiver_charts_validation_appApp.swift
+//  quiver-charts-validation-app
+//
+//  Created by Wayne Bishop on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct quiver_charts_validation_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
